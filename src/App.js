e74074cb536bcd3f39f4f9fd6516733fa2b8a314
Khalifaomeiza <khalifabdul96@gmail.com>
2023-05-15 11:41:24 +0100

    Update Task Button Functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,163 +1,195 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faCircleCheck, faTrashCan, faPen} from '@fortawesome/free-solid-svg-icons'
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faTrashCan,
+  faPen,
+} from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 
 function App() {
-  // Todo List States
+  // Todo List Dummy States
   const [todo, setTodo] = useState([
-    // creating default task 
-    {id:1, title: "Task 1", status: false},
-    {id:2, title: "Task 2", status: true}
+    // creating default task
+    { id: 1, title: "Task 1", status: false },
+    { id: 2, title: "Task 2", status: true },
+    { id: 3, title: "Task 3", status: true },
   ]);
 
+  // Temporary or Initial States for Adding Task
+  const [newTask, setNewTask] = useState("");
 
-  // Temporary or Initial States
-  const [newTask, setNewTask] = useState('');
-  const [updateTodo, setUpdateTodo] = useState('');
-
+  // Temporary or Initial States for Updating Task
+  const [updateTodoData, setUpdateTodoData] = useState(""); //setting states variable for Update
 
   // Add Todo task
-  const addTask = () => {
-    if (newTask.trim){
-      let num = todo.length + 1
-      let newEntry = {id: num, title: newTask, status: false}
-      setTodo([...todo, newEntry])
-      console.log([...todo, newEntry])
-      setNewTask('')
+  const addTask = (e) => {
+    if (newTask) {
+      let num = todo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setTodo([...todo, newEntry]);
+      setNewTask("");
     }
-
- 
-  }
+  };
 
   //Delete Todo task
-  const deleteTask =(id) => {
-    let newTasks =todo.filter (task => task.id !==id)
-    setTodo(newTasks)
-    
-  }
+  const deleteTask = (id) => {
+    let newTodoList = todo.filter((task) => task.id !== id);
+    setTodo(newTodoList);
+  };
 
   //mark task as done or completed
-    const markDone =(id) => {
-      let newTask =todo.map (task =>{
-
-        if(task.id ==id) {
-          return ({...task, status: !task.status})
-        }
-        return task
-      })
-      setTodo(newTask)
-
-  }
+  const markDone = (id) => {
+    let taskStatus = todo.map((task) => {
+      if (task.id === id) {
+        //returning the task as an object but toggles the status between the options of true and false.
+        return { ...task, status: !task.status };
+      }
+      return task;
+    });
+    setTodo(taskStatus);
+  };
 
   //cancel update
-  const cancelUpdate =(id) => {
+  const cancelUpdate = (id) => {
+    // setting the cancel update to the temp state of null
+    setUpdateTodoData("");
+  };
 
-  }
-  
-  //change task for update. This listens to any change in an event.
-  const changeTask =(e) => {
-
-  }
+  //edit task for update. This listens to any change in an event.
+  const editTask = (e) => {
+    let newEntry = {
+      id: updateTodoData.id,
+      title: e.target.value,
+      status: updateTodoData.status ? true : false,
+    };
+    setUpdateTodoData(newEntry);
+  };
 
   //update task
-  const updateTask=() => {
-
-  }
+  const updateTask = () => {
+    let filterRecords = [...todo].filter(
+      (task) => task.id !== updateTodoData.id
+    );
+    let updateTodoList = [...filterRecords, updateTodoData];
+    setTodo(updateTodoList);
+    setUpdateTodoData("");
+  };
 
   return (
     <div className="container App">
-
-      <br/>
+      <br />
       <h2>React Todo List </h2>
-      <br/>
-      <br/>
+      <br />
+      <br />
 
-      {/* Creating a form to update Task */}
-      <div className='row'>
-        <div className='col'>
-          <input className='form-control form-control-lg'></input>
-        </div>
-        
-        <div className='col-auto'>
-          <button className='btn btn-lg' style={{marginRight: 20, backgroundColor: '#572e9c', color: 'white'}}>Update</button>
-          <button className='btn btn-lg' style={{color: '#572e9c', border: '1px solid ' }}> Cancel</button>
+      {/* UPDATE TASK:  Creating a form to update Task */}
+      <div className="row">
+        <div className="col">
+          <input
+            className="form-control form-control-lg"
+            value={updateTodoData && updateTodoData.title}
+            onChange={(e) => editTask(e)}
+          ></input>
         </div>
 
+        <div className="col-auto">
+          <button
+            onClick={updateTask}
+            className="btn btn-lg"
+            style={{
+              marginRight: 20,
+              backgroundColor: "#572e9c",
+              color: "white",
+            }}
+          >
+            Update
+          </button>
+          <button
+            onClick={cancelUpdate}
+            className="btn btn-lg"
+            style={{ color: "#572e9c", border: "1px solid " }}
+          >
+            Cancel
+          </button>
+        </div>
       </div>
-      <br/>
-
+      <br />
 
       {/* Creating a Form to Add new Task */}
-      <div className='row'>
-        <div className='col'>
-          <input 
-          value= {newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          className='form-control form-control-lg'>
-          </input>
+      <div className="row">
+        <div className="col">
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className="form-control form-control-lg"
+          ></input>
         </div>
 
-        <div className='col-auto'>
-          <button className='btn btn-lg btn-success' onClick={addTask}>
+        <div className="col-auto">
+          <button className="btn btn-lg btn-success" onClick={addTask}>
             Add Task
           </button>
         </div>
-        
       </div>
-      <br/>
+      <br />
 
       {/* Displaying Todo Record */}
 
       {/* setting up a condition that display "No Tasks" if Todo is empty and the number of Task is null" */}
-      {todo && todo.length ? '': 'No Tasks...'} 
+      {todo && todo.length ? "" : "No Tasks..."}
 
       {/* sorting through the id of all task in the record to ensure a chronological arrangement */}
       {todo
-      .sort((a, b) => a.id > b.id ? 1: -1)
-      .map((task, index) => {
-        console.log (task, index)
+        .sort((a, b) => (a.id > b.id ? 1 : -1))
+        .map((task, index) => {
+          // console.log (task, index)
 
-        return (
-          <React.Fragment>
+          return (
+            <React.Fragment>
+              <div className="col taskContainer">
+                {/* declaring conditions for styling 'done' or completed task while 'Not Completed task hold no styling  */}
+                <div className={task.status ? "done" : ""}>
+                  <span className="taskNumber">{index + 1}</span>
+                  <span className="taskText">{task.title}</span>
+                </div>
 
-            <div className="col taskContainer">
+                <div className="iconsContainer">
+                  {/* MarkDone icon */}
+                  <span
+                    title="Completed/Not Completed"
+                    onClick={(e) => markDone(task.id)}
+                  >
+                    <FontAwesomeIcon icon={faCircleCheck} />
+                  </span>
 
-              {/* declaring conditions for styling 'done' or completed task while 'Not Completed task hold no styling  */}
-              <div className= {task.status? 'done': ''}>
+                  {/* Making the Edit icon dependent on task states. Thus if task state is true (Completed) we get null (No Edit icon) */}
 
-                <span className="taskNumber">{index + 1}</span>
-                <span className="taskText">{task.title}</span>
+                  {task.status ? null : (
+                    <span
+                      title="Edit"
+                      onClick={() =>
+                        setUpdateTodoData({
+                          id: task.id,
+                          title: task.title,
+                          status: task.status ? true : false,
+                        })
+                      }
+                    >
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                  )}
 
+                  {/* Delete Icon */}
+                  <span title="Delete" onClick={() => deleteTask(task.id)}>
+                    <FontAwesomeIcon icon={faTrashCan} />
+                  </span>
+                </div>
               </div>
-
-              <div className="iconsContainer">
-
-                {/* MarkDone icon */}
-                <span title ='Completed/Not Completed' onClick={(e) => markDone(task.id)}>
-                <FontAwesomeIcon icon={faCircleCheck} /></span>
-                
-                {/* Edit Icon */}
-                <span title='Edit'>
-                <FontAwesomeIcon icon={faPen} /></span>
-
-                {/* Delete Icon */}
-                <span title='Delete' onClick={()=> deleteTask(task.id)}>
-                <FontAwesomeIcon icon={faTrashCan} /></span>
-
-              </div>
-
-            </div>
-                      
-                    
-
-          </React.Fragment>
-        )
-
-      })}
-
-
+            </React.Fragment>
+          );
+        })}
     </div>
   );
 }
